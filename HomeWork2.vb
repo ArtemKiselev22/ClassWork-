a887240745e12078405bdfef87ee86cfc93f0031
Artem-Kiselev 22-ИС <122738926+ArtemKiselev22@users.noreply.github.com>
2023-01-26 20:09:17 +0300

    Add files via upload

---
+++ HomeWork2.vb
@@ -0,0 +1,20 @@
+Imports System
+Imports System.ComponentModel
+Imports System.Security.Cryptography
+
+Module Program
+    Sub Main(args As String())
+
+        Dim a, c As Integer
+        a = 1
+
+        Do
+            Console.WriteLine("Введите множитель")
+            c = Console.ReadLine
+            a = a * c
+        Loop Until c = 1
+
+        Console.WriteLine("Произведение = {0} ", a)
+
+    End Sub
+End Module
